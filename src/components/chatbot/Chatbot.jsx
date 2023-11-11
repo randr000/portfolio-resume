df65226aefb33e8bbaa0664de2e07dcad0ea0a1e
Raul Andrial <rauljandrial@gmail.com>
2023-11-10 21:46:50 -0500

    Chatbot position bottom right

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -20,32 +20,34 @@ const Chatbot = () => {
     };
 
     return (
-        <div className="container mt-5">
-          <div className="chat-container">
-            <div className="chat-box">
-              {messages.map((message, index) => (
-                <div key={index} className={`message ${message.sender}`}>
-                  {message.text}
+        
+            <div className="container-fluid">
+              <div className="chat-container">
+                <div className="chat-box">
+                  {messages.map((message, index) => (
+                    <div key={index} className={`message ${message.sender}`}>
+                      {message.text}
+                    </div>
+                  ))}
                 </div>
-              ))}
+                <div className="input-group mt-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Type your message..."
+                    value={newMessage}
+                    onChange={handleInputChange}
+                  />
+                  <button
+                    className="btn btn-primary"
+                    onClick={handleSendMessage}
+                  >
+                    Send
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="input-group mt-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Type your message..."
-                value={newMessage}
-                onChange={handleInputChange}
-              />
-              <button
-                className="btn btn-primary"
-                onClick={handleSendMessage}
-              >
-                Send
-              </button>
-            </div>
-          </div>
-        </div>
+        
       );
     };
 
