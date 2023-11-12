0a8ee6f44e6c2b76f2058f12527590003933e2cf
Raul Andrial <rauljandrial@gmail.com>
2023-11-12 15:49:58 -0500

    Made chatbot more responsive to different screen sizes

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Form, Button, Card } from 'react-bootstrap';
+import { Form, Button, Card, Container } from 'react-bootstrap';
 import BIChatLeftDots from '../bootstrap-icons/BIChatLeftDots'
 import BIRobot from '../bootstrap-icons/BIRobot'
 
@@ -45,58 +45,55 @@ const Chatbot = () => {
     }
 
     return (
-        <div className="me-3 mb-2" style={{position: "fixed", zIndex: 100,  right: 0, bottom: 0}}>
-          {
-            showChat ?
-
-            <Card className="bg-white">
-                <Card.Header className="bg-primary text-white">
-                  <div className="d-flex justify-content-between" onClick={handleToggleChat}>
-                    <p className="h3 google-font-600 my-auto">Raul AI</p>
-                    <p className="h3 google-font-1000 pb-3" style={{cursor: "pointer"}}>_</p>
-                  </div>
-                </Card.Header>
-                <Card.Body>
-                      <div ref={containerRef} className="chat-box rounded">
-                        {messages.map((message, index) => (
-                          !message.loading ?
-                            <div key={index} className={`message ${message.sender} w-75 rounded m${message.sender == 'bot' ? 'e' : 's'}-auto`}>
-                              {message.text}
-                            </div> :
-                            <div key={index} className={`message ${message.sender} w-75 rounded m${message.sender == 'bot' ? 'e' : 's'}-auto`}>
-                              <div className="d-flex align-items-center">
-                                <div className="message-bubble animation-delay-1 bg-secondary"></div>
-                                <div className="message-bubble animation-delay-2 bg-secondary"></div>
-                                <div className="message-bubble animation-delay-3 bg-secondary"></div>
+          <div className="me-1 mb-1 me-lg-3 mb-lg-2 col-xs-12 col-sm-12 col-lg-6" style={{position: "fixed", zIndex: 100,  right: 0, bottom: 0}}>
+            {
+              showChat ?
+              <Card className="bg-white">
+                  <Card.Header className="bg-primary text-white">
+                    <div className="d-flex justify-content-between" onClick={handleToggleChat}>
+                      <p className="h3 google-font-600 my-auto">Raul AI</p>
+                      <p className="h3 google-font-1000 pb-3" style={{cursor: "pointer"}}>_</p>
+                    </div>
+                  </Card.Header>
+                  <Card.Body>
+                        <div ref={containerRef} className="chat-box rounded">
+                          {messages.map((message, index) => (
+                            !message.loading ?
+                              <div key={index} className={`message ${message.sender} w-75 rounded m${message.sender == 'bot' ? 'e' : 's'}-auto`}>
+                                {message.text}
+                              </div> :
+                              <div key={index} className={`message ${message.sender} w-75 rounded m${message.sender == 'bot' ? 'e' : 's'}-auto`}>
+                                <div className="d-flex align-items-center">
+                                  <div className="message-bubble animation-delay-1 bg-secondary"></div>
+                                  <div className="message-bubble animation-delay-2 bg-secondary"></div>
+                                  <div className="message-bubble animation-delay-3 bg-secondary"></div>
+                                </div>
+                                <p className="">Raul AI is typing...</p>
                               </div>
-                              <p className="">Raul AI is typing...</p>
-                            </div>
-                        ))}
-                      </div>
-                      <Form className="input-group mt-3" onSubmit={(e) => handleSubmit(e)}>
-                        <Form.Control
-                          type="text"
-                          className="form-control"
-                          placeholder="Type your message..."
-                          value={newMessage}
-                          onChange={handleInputChange}
-                        />
-                        <Button type="submit" variant="primary">Send</Button>
-                      </Form>
-                </Card.Body>
-            </Card> :
-
-            <div className="d-flex text-primary" onClick={handleToggleChat} style={{cursor: "pointer"}}>
-              <div className="mt-3">
-                <BIRobot/>
+                          ))}
+                        </div>
+                        <Form className="input-group mt-3" onSubmit={(e) => handleSubmit(e)}>
+                          <Form.Control
+                            type="text"
+                            className="form-control"
+                            placeholder="Type your message..."
+                            value={newMessage}
+                            onChange={handleInputChange}
+                          />
+                          <Button type="submit" variant="primary">Send</Button>
+                        </Form>
+                  </Card.Body>
+              </Card> :
+              <div className="d-flex text-primary justify-content-end pe-1" onClick={handleToggleChat} style={{cursor: "pointer"}}>
+                <div className="mt-3">
+                  <BIRobot/>
+                </div>
+                <div className="mb-2">
+                  <BIChatLeftDots style={{width: "100px", height: "100px"}}/>
+                </div>
               </div>
-              <div className="mb-2">
-                <BIChatLeftDots style={{width: "100px", height: "100px"}}/>
-              </div>
-            </div>
-          }
-        </div>
-          
+            }
+          </div>
       );
     };
 
